99a7f3dab5f9ed8b51e87e0d26c4c23f124d7b66
destiny <theimpactindex@gmail.com>
2025-05-20 20:02:33 +0100

    added the create and project

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -1,31 +1,39 @@
-'use client';
-import {useEffect, useState} from 'react';
-import { useRouter } from 'next/navigation';
+"use client";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
 export default function AdminDashboard() {
-
   const router = useRouter();
 
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
-  const [error, setError] = useState('');
-  
-useEffect(() => {
+  const [error, setError] = useState("");
+
+  useEffect(() => {
     const fetchUser = async () => {
-      setLoading(true)
+      setLoading(true);
       try {
-        const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URI}/api/users/me`, {headers: {Authorization: `Bearer ${localStorage.getItem('accesstoken')}`}});
+        const response = await fetch(
+          `${process.env.NEXT_PUBLIC_BASE_URI}/api/users/me`,
+          {
+            headers: {
+              Authorization: `Bearer ${localStorage.getItem("accesstoken")}`,
+            },
+          }
+        );
         if (!response.ok) {
-          throw new Error('Network response was not ok');
+          throw new Error("Network response was not ok");
         }
-        const {data} = await response.json();
+        const { data } = await response.json();
         setLoading(false);
         setUser(data);
       } catch (error) {
-        console.log(error)
+        console.log(error);
         setLoading(false);
-        setError('Attention: You are not authorized to view this page. Please log in as an admin.');
+        setError(
+          "Attention: You are not authorized to view this page. Please log in as an admin."
+        );
         setTimeout(() => {
-          router.push('/admin/login');
+          router.push("/admin/login");
         }, 1000);
       } finally {
         setLoading(false);
@@ -33,24 +41,31 @@ useEffect(() => {
     };
 
     fetchUser();
-}, [])
+  }, []);
 
-
-if(!user) {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      {loading ? (
-        <p className="text-gray-700">Loading...</p>
-      ) : (
-        <p className="text-red-500">{error}</p>
-      )}
-    </div>
-  );
-}
+  if (!user) {
+    return (
+      <div className="min-h-screen flex items-center justify-center bg-gray-100">
+        {loading ? (
+          <p className="text-gray-700">Loading...</p>
+        ) : (
+          <p className="text-red-500">{error}</p>
+        )}
+      </div>
+    );
+  }
   return (
     <div className="min-h-screen p-8 bg-gray-100">
-      <h1 className="text-3xl font-bold mb-4">Welcome to the Admin Dashboard</h1>
-      <p className="text-gray-700">You are now logged in!</p>
+      <h1 className="text-3xl font-bold mb-4">Welcome back, </h1>
+
+      <div className="flex flex-col items-center justify-center min-h-screen">
+        <a className="text-[#888] bg-[#f8f8f8] py-3 px-5 rounded-lg " href="/admin/category">
+          Create Category
+        </a>
+        <a className="text-[#888] bg-[#f8f8f8] py-3 px-5 rounded-lg " href="/admin/projects">
+          Create Project
+        </a>
+      </div>
     </div>
   );
 }
